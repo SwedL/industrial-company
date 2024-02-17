8cc0f4627379a20ad42f053eef606fc358fb7fb1
Алексей <supersega@mail.ru>
2024-02-17 15:01:10 +0500

    suspended HTMX form add employee

--- structure/urls.py
+++ structure/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('structure-company/', StructureCompanyTemplateView.as_view(), name='structure_company'),
     path('employees-list/<int:position_id>/', EmployeesListView.as_view(), name='employees_list'),
     path('add-employee/', AddEmployee.as_view(), name='add_employee'),
+    path('create-employee/', AddEmployee.as_view(), name='create_employee'),
     # path('employees-list/<int:position_id>/<int:page>/', employees_list, name='employees_list'),
 ]
--- structure/views.py
+++ structure/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.decorators.http import require_http_methods
 from django.views.generic import TemplateView, View, ListView, CreateView
 
 from structure.forms import UserLoginForm
@@ -18,46 +19,13 @@ class UserLoginView(LoginView):
     form_class = UserLoginForm
     template_name = 'structure/login.html'
 
-    # def get_success_url(self):
-    #     return reverse_lazy('structure:structure_company')
-
 
 class StructureCompanyTemplateView(TemplateView):
-    template_name = 'structure/structure-company.html'
-
-
-# class PositionView(View):
-#     def get(self, request, position_id):
-#         position = Position.objects.filter(id=position_id).first()
-#         employee = position.employee_set.all().first()
-#         manager_name = f'{employee.last_name} {employee.first_name[0]}.{employee.patronymic[0]}.'
-#
-#         context = {
-#             'position_name': position.name,
-#             'manager_name': manager_name,
-#         }
-#         return render(request, 'structure/employees-list.html', context=context)
-
-
-def employees_list(request, position_id=None, page=1):
-    employees = Employee.objects.filter(position=position_id) if position_id else Employee.objects.all()
-    per_page = 20
-    paginator = Paginator(employees, per_page)
-    paginator.get_elided_page_range(number=10, on_each_side=3, on_ends=2)
-    employees_paginator = paginator.page(page)
-
-    context = {
-        'title': 'Список сотрудников',
-        'employees': employees_paginator,
-        'position_id': position_id,
-        'number': page,
-    }
-
-    return render(request, 'structure/employees-list.html', context)
+    template_name = 'structure/structure_company.html'
 
 
 class EmployeesListView(ListView):
-    template_name = 'structure/employees-list.html'
+    template_name = 'structure/employees_list.html'
     context_object_name = 'employees'
     paginate_by = 20
 
@@ -68,16 +36,24 @@ class EmployeesListView(ListView):
         context = super().get_context_data(**kwargs)
         page = context['page_obj']
         context['paginator_range'] = page.paginator.get_elided_page_range(page.number)
-        # context['position_id'] = self.kwargs['position_id']
         return context
 
 
 class AddEmployee(CreateView):
     form_class = AddEmployeeForm
-    template_name = 'structure/add-employee.html'
+    template_name = 'structure/add_employee.html'
     success_url = reverse_lazy('structure:add_employee')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['employees'] = Employee.objects.filter(position=None)
         return context
+
+    # def post(self, request, *args, **kwargs):
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid:
+    #         employee = form.save()
+    #     else:
+    #         return self.render_to_response(self.get_context_data(form=form))
+    #     return render(request, 'structure/employee_detail.html', {'employee': employee})
+
