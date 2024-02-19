bac65e9c34906d7f583d79f8b68fe5f3f00a30d4
Алексей <supersega@mail.ru>
2024-02-19 22:28:40 +0500

    add common_sql to the views

--- structure/views.py
+++ structure/views.py
@@ -17,6 +17,9 @@ from django.urls import reverse
 from .forms import AddEmployeeForm
 
 
+common_sql = []
+
+
 class UserLoginView(LoginView):
     form_class = UserLoginForm
     template_name = 'structure/login.html'
@@ -25,17 +28,29 @@ class UserLoginView(LoginView):
 class StructureCompanyTemplateView(TemplateView):
     template_name = 'structure/structure_company.html'
 
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        common_sql.clear()
+        return self.render_to_response(context)
+
 
 class EmployeesView(View):
     paginate_by = 25
 
-    def get(self, request, position_id, order_by, direction):
-        form = SearchEmployeeForm()
+    def get(self, request, position_id: int, order_by: str, direction: str):
+        if common_sql:
+            form = SearchEmployeeForm(common_sql[1])
+        else:
+            form = SearchEmployeeForm()
 
         if direction == 'descend':
             order_by += ' DESC'
 
-        sql = f'SELECT ROW_NUMBER() OVER(ORDER BY {order_by}) AS num, * FROM structure_employee WHERE position_id = {position_id} ORDER BY {order_by}'
+        if common_sql:
+            sql = common_sql[0]
+        else:
+            sql = f'SELECT ROW_NUMBER() OVER(ORDER BY {order_by}) AS num, * FROM structure_employee WHERE position_id = {position_id} ORDER BY {order_by}'
+
         employees_list = Employee.objects.raw(sql)
 
         paginator = Paginator(employees_list, 20)
@@ -50,25 +65,36 @@ class EmployeesView(View):
         }
         return render(request, 'structure/department.html', context=context)
 
-    def post(self, request, position_id, order_by, direction):
-        form = SearchEmployeeForm()
-        where_list = ['last_name', 'first_name', 'patronymic', 'employment_date', 'salary']
-        where_dict = {i: request.POST[i] for i in where_list}
+    def post(self, request, position_id: int, order_by: str, direction: str):
 
-        normal_list = [f'{k} LIKE "%{v}%"' for k, v in where_dict.items() if v]
-        normal_list.insert(0, f'WHERE position_id = {position_id}')
+        search_key_list = ['last_name', 'first_name', 'patronymic', 'employment_date', 'salary']
+        request_dict_from_the_search_key_list = {i: request.POST[i] for i in search_key_list}
 
-        sql_where = ' AND '.join(normal_list)
+        list_filters_for_sql = [f'{k} LIKE "%{v}%"' for k, v in request_dict_from_the_search_key_list.items() if v]
+        if not list_filters_for_sql:
+            common_sql.clear()
+
+        # TODO replace sql in common_sql on WHERE
+        list_filters_for_sql.insert(0, f'WHERE position_id = {position_id}')
+        where_for_sql = ' AND '.join(list_filters_for_sql)
 
         if direction == 'descend':
             order_by += ' DESC'
 
-        sql = f'SELECT ROW_NUMBER() OVER(ORDER BY {order_by}) AS num, * FROM structure_employee {sql_where} ORDER BY {order_by}'
+        sql = f'SELECT ROW_NUMBER() OVER(ORDER BY {order_by}) AS num, * FROM structure_employee {where_for_sql} ORDER BY {order_by}'
+        common_sql.insert(0, sql)
+        common_sql.insert(1, request.POST)
+
         employees_list = Employee.objects.raw(sql)
 
         paginator = Paginator(employees_list, 20)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
+
+        if common_sql:
+            form = SearchEmployeeForm(common_sql[1])
+        else:
+            form = SearchEmployeeForm()
 
         context = {
             'employees': page_obj,
