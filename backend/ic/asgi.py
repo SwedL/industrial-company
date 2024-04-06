366086ba62779e91b159a8e032f2d34a6356399a
Alexey Osminin <supersega@mail.ru>
2024-04-06 17:09:45 +0500

    docker-compose complete

--- backend/ic/asgi.py
+++ backend/ic/asgi.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+import structure.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-import structure.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels_chat.settings')
 
